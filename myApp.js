a64ad58107812a08969605bb306b82b6b69bd41d
Glitch (sage-flannel) <none>
2019-10-05 20:38:40 +0000

    🙆🐀 Checkpoint ./myApp.js:7351991/13

--- myApp.js
+++ myApp.js
@@ -12,7 +12,7 @@ var app = express();
 console.log('Hello World');
 
 /** 2) A first working Express Server */
-app.get('/root', (req,res)=>console.log(res.send('Hello Express')))
+app.get('/root', (req,res)=>res.send('Hello Express'))
 
 /** 3) Serve an HTML file */
 
