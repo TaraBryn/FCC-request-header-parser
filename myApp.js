cc17fa4e783f1614f974bfe458052abbcc29d981
Glitch (sage-flannel) <none>
2019-10-06 05:06:03 +0000

    🚀🚨 Checkpoint ./myApp.js:7351991/474

--- myApp.js
+++ myApp.js
@@ -51,7 +51,11 @@ app.get('/:word/echo',(req,res)=>res.json({echo: req.params.word}))
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 app.route('/name')
-.get((req,res)=>res.json({name: `${req.query.first} ${req.query.last}`}))
+.get(function(req,res,next){
+  res.json({name: `${req.query.first} ${req.query.last}`});
+  next();
+})
+.post((req,res)=>res.json({name: `${req.body.first} ${req.body.last}`}));
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
