17b238ffefba27b886f0e08cca088a5c9528eb58
Glitch (sage-flannel) <none>
2019-10-06 00:48:44 +0000

    🚈⛵️ Checkpoint ./myApp.js:7351991/187

--- myApp.js
+++ myApp.js
@@ -30,8 +30,7 @@ app.use(
   function(req, res, next){
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
-  },
-  express.static(__dirname + '/public')
+  }
 )
  
 /** 7) Root-level Middleware - A logger */
